b44d7c253ad534227d9cfa7a20347bc41cffba1f
IdanG97 <138139373+IdanG97@users.noreply.github.com>
2023-09-20 17:53:28 +0300

    Create car.js

---
+++ car.js
@@ -0,0 +1,27 @@
+class Car {
+    constructor(car_brand, car_model, car_year, car_color, car_mileage) {
+        this.brand = car_brand,
+            this.model = car_model,
+            this.year = car_year,
+            this.color = car_color,
+            this.mileage = car_mileage
+    }
+
+    set_brand(modified_brand) {
+        this.brand = modified_brand;
+    }
+    set_model(modified_model) {
+        this.model = modified_model;
+    }
+    set_year(modified_year) {
+        if (modified_year > 1900) {
+            this.year = modified_year;
+        }
+    }
+    set_color(modified_color) {
+        this.color = modified_color;
+    }
+    set_mileage(modified_mileage) {
+        this.mileage = modified_mileage;
+    }
+}
